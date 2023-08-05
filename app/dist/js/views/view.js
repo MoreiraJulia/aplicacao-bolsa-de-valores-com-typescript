fc328f7c4ce37b57ae1b6894aefc203dfd2bae58
Julia Aparecida Moreira <julia.aparecida25@gmail.com>
2023-08-05 18:05:34 -0300

    Atualização na organização da estrutura do projeto

--- app/dist/js/views/view.js
+++ app/dist/js/views/view.js
@@ -1,19 +1,15 @@
 export class View {
-    constructor(seletor, escapar) {
-        this.escapar = false;
+    constructor(seletor) {
         const elemento = document.querySelector(seletor);
         if (elemento) {
             this.elemento = elemento;
         }
-        if (escapar) {
-            this.escapar = escapar;
+        else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`);
         }
     }
     update(model) {
         let template = this.template(model);
-        if (this.escapar) {
-            template = template.replace(/<script>[\s\S]*?<\/script>/, '');
-        }
         this.elemento.innerHTML = template;
     }
 }
